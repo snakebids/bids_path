b1b01311c3193d7dafaa953507e39d231a306068
Peter Van Dyken <pvandyk2@uwo.ca>
2023-06-29 23:19:25 -0400

    Try new poetry plugin

--- bids_path/__init__.py
+++ bids_path/__init__.py
@@ -3,10 +3,7 @@ __submodules__ = ["main"]
 __version__ = "0.0.0"
 
 # <AUTOGEN_INIT>
-from bids_path.main import (
-    bids,
-    print_boilerplate,
-)
+from bids_path.main import bids, print_boilerplate
 
 __all__ = ["bids", "print_boilerplate"]
 
